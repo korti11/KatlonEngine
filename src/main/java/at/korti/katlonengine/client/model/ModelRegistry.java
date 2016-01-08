734005ac8712de11baa289c7ede587f2f42947d7
Korti <lucas.korthauer@gmail.com>
2016-01-08 09:19:25 +0100

    Models can now registered in the model registry.

---
+++ src/main/java/at/korti/katlonengine/client/model/ModelRegistry.java
@@ -0,0 +1,52 @@
+package at.korti.katlonengine.client.model;
+
+import at.korti.katlonengine.client.model.parser.WavefrontParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Korti on 08.01.2016.
+ */
+public class ModelRegistry {
+
+    private static ModelRegistry instance;
+
+    private Map<String, Model> registry;
+
+    private ModelRegistry() {
+        registry = new HashMap<>();
+    }
+
+    public static ModelRegistry instance() {
+        if (instance == null) {
+            instance = new ModelRegistry();
+        }
+        return instance;
+    }
+
+    public static void addModel(String id, Model model) {
+        if (!isKeyInUse(id)) {
+            instance().registry.put(id, model);
+        }
+    }
+
+    public static void addModelForWaveFrontFile(String id, String pathToFile) {
+        if(!isKeyInUse(id)) {
+            WavefrontParser parser = new WavefrontParser(pathToFile);
+            addModel(id, parser.parse());
+        }
+    }
+
+    public static Model getModel(String id) {
+        if (isKeyInUse(id)) {
+            return instance().registry.get(id);
+        }
+        return null;
+    }
+
+    public static boolean isKeyInUse(String id) {
+        return instance().registry.containsKey(id);
+    }
+
+}
